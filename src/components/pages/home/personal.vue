2dfa7e345111d608dfad8d25e100b4854cda5db0
taro <3128208075@qq.com>
2017-03-22 11:34:27 +0800

    增加“我的”页面

--- src/components/pages/home/personal.vue
+++ src/components/pages/home/personal.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
-    我的
+    <ul v-if="!authenticated" class="mdl-list">
+      <list-item text="登录" />
+      <list-item text="注册" />
+    </ul>
+    <ul v-if="authenticated" class="mdl-list">
+      <list-item text="我的等级" />
+      <list-item text="我的日记" />
+      <list-item text="我的收藏" />
+      <list-item text="我的服务" />
+    </ul>
+    <ul class="mdl-list">
+      <list-item text="设置" :divider="false" />
+    </ul>
   </div>
 </template>
+
+<script>
+  export default {
+    components: {
+      "list-item": require("components/ui/list-item.vue")
+    },
+    computed: {
+      authenticated() {
+        return this.$store.state.user.authenticated;
+      }
+    }
+  };
+
+</script>
---
+++ src/components/ui/list-item.vue
@@ -0,0 +1,25 @@
+<template>
+  <li class="mdl-list__item" :class="{'mdl-list__item--bottom-divider': divider}">
+    <span class="mdl-list__item-primary-content">{{ text }}</span>
+  </li>
+</template>
+
+<script>
+  export default {
+    computed: {
+      dividerClass() {
+        return {
+          "mdl-list__item--bottom-divider": this.divider
+        };
+      }
+    },
+    props: {
+      text: String,
+      divider: {
+        type: Boolean,
+        default: true
+      }
+    }
+  };
+
+</script>
