42be13db73782f1fae03b83c1206069abf2f8b1e
xiahdfsq <2435068331@qq.com>
2017-06-02 17:57:58 +0800

    订阅

--- src/components/pages/dynamic/subscrible.vue
+++ src/components/pages/dynamic/subscrible.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="page" title="订阅">
-    <span>我的订阅</span>
+  <div class="page" title="我的订阅" actions='[{"text":"添加","clickHandler":"add-subscrible"}]'>
+    <ul class="sub-list">
+      <li class="sub-item">
+        
+        <div class="material-icons mdl-badge mdl-badge--overlap" data-badge="1"><img src="/img/default-avatar.png" width="36" height="36" /></div>
+        <div class="item-content">
+          <span class="sub-name">陈翔6点半</span>
+          <span class="sub-info">陈翔6点半陈翔6点半陈翔6点半</span>
+        </div>
+        <span class="sub-time">2017-6-1</span>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
@@ -15,6 +25,13 @@
 
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+  @import "~scss/main.scss";
+.sub-item{
+  @include flex-row;
+  padding: 15px;
+}
+.item-content{
+  flex:1;
+}
 </style>
