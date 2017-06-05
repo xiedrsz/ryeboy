952a769a05f512d9ccc21ea96622b7da286e35fb
xiahdfsq <2435068331@qq.com>
2017-06-06 00:07:23 +0800

    添加申请

--- src/components/pages/dynamic/group/group-invite.vue
+++ src/components/pages/dynamic/group/group-invite.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="page" title="邀请好友">
-    <span>邀请好友</span>
+    <label class="invite-list">
+	    <div class="invite-item">
+	      <img src="/img/default-avatar.png" class="lazyload" width="48" height="48" />
+	      <span class="invite-content">爱一个人默默的哭 </span>
+	      <!-- <span class="manage-follow" @click="followIt">订阅</span> -->
+	      <input type="checkbox">
+	      <i class="material-icons">{{checked?'radio_button_unchecked':'check_circle'}}</i>
+	    </div>
+	</label>
   </div>
 </template>
 <script>
   export default {
-    components: {
-
+    data() {
+    	return{
+    		checked:true
+    	}
+    	
     },
     computed: {}
   };
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+  @import "~scss/main.scss";
+.invite-item {
+    @include flex-row;
+    @include flex-vertical-center;
+    padding: 5px 15px;
+    border-bottom: 1px solid $color-divider;
+    .invite-content {
+      flex: 1;
+      @include flex-column;
+      padding-left: 15px;
+    }
+    input{
+    	visibility: hidden;
+    }
+  }
 </style>
