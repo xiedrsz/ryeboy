828ec03e12a3f27c8ac1443b02b35cb9a770904c
taro <3128208075@qq.com>
2017-08-02 08:54:31 +0800

    修改api地址配置逻辑

--- src/js/config.js
+++ src/js/config.js
@@ -1,17 +1,20 @@
 const debug = true;
-const useLocalApi = true;
 
 var configFile, apiAddress;
 try {
   configFile = require("./config.json");
-  apiAddress = `https://${configFile.api_host}:${configFile.api_port}`;
+  if (configFile.debug_useRemoteApi) {
+    apiAddress = `https://${configFile.debug_remoteApiAddress}`;
+  } else {
+    apiAddress = `https://${configFile.debug_localApiAddress}`;
+  }
 } catch (error) {
   apiAddress = "https://127.0.0.1:2443";
 }
 
 const config = {
   debug,
-  apiAddress: debug ? useLocalApi ? apiAddress : "https://114.215.131.176:2443" : "https://api.ryeboy.org",
+  apiAddress: debug ? apiAddress : "https://api.ryeboy.org",
   ossAddress: "http://ryeboy-app-apk-1.oss-cn-qingdao.aliyuncs.com",
   pageSize: {
     normal: 10,
